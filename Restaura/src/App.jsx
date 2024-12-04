eea0b5016d68ba809c36f26107a2986ad4d4f421
KalpeshKumar-09 <sutharkalpesh101@gmail.com>
2024-12-04 13:23:13 +0530

    Other sections are added

--- Restaura/src/App.jsx
+++ Restaura/src/App.jsx
@@ -1,11 +1,19 @@
+import About from "./components/About";
+import Dishes from "./components/Dishes";
+import Expertise from "./components/Expertise";
 import HeroSection from "./components/HeroSection";
+import Mission from "./components/Mission";
 import NavbarSection from "./components/NavbarSection";
 
 function App() {
   return (
     <main className="overflow-y-hidden text-neutral-200 antialiased">
-      <NavbarSection />
       <HeroSection />
+      <NavbarSection />
+      <Dishes />
+      <About />
+      <Mission />
+      <Expertise />
     </main>
   );
 }
